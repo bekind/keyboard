e66604cc21497c5ca04ce9ea4801deb5c684005a
alice <alice_hqin@126.com>
2015-11-05 11:26:08 +0800

    Initial Commit

---
+++ KeyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyBoard
+//
+//  Created by dblab on 15-11-5.
+//  Copyright (c) 2015年 dblab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KeyBoard
+//
+//  Created by dblab on 15-11-5.
+//  Copyright (c) 2015年 dblab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
